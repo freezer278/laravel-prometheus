e4be818293b068f2066423789e929d8c726673da
Volodymyr Morozov <v.morozov@promodo.com>
2025-06-04 15:14:32 +0300

    config improvement

--- config/laravel-prometheus.php
+++ config/laravel-prometheus.php
@@ -2,7 +2,7 @@
 
 return [
     // namespace is used to prefix the prometheus metrics, can be any string value
-    'namespace' => env('PROMETHEUS_NAMESPACE', ''),
+    'namespace' => env('PROMETHEUS_NAMESPACE', env('APP_NAME', 'laravel')),
     // type of storage for keeping metrics data (known values: redis, in_memory, apcu). redis - works best now.
     'storage_type' => env('PROMETHEUS_STORAGE_TYPE', 'redis'),
     // redis connection for prometheus metrics (used only when storage_type == redis)
