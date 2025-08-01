e2b8b47d724a20c0ee784e588e25a809bf2e3b2a
Volodymyr Morozov <v.morozov@promodo.com>
2025-08-01 09:57:11 +0300

    updated default prometheus namespace value to be the standard one (laravel)

--- config/laravel-prometheus.php
+++ config/laravel-prometheus.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    // namespace is used to prefix the prometheus metrics, can be any string value
-    'namespace' => env('PROMETHEUS_NAMESPACE', env('APP_NAME', 'laravel')),
+    // namespace is used to prefix the prometheus metrics, can be any string value, but it is recommended to keep a default value
+    'namespace' => env('PROMETHEUS_NAMESPACE', 'laravel'),
     // type of storage for keeping metrics data (known values: redis, in_memory, apcu). redis - works best now.
     'storage_type' => env('PROMETHEUS_STORAGE_TYPE', 'redis'),
     // redis connection for prometheus metrics (used only when storage_type == redis)
@@ -23,6 +23,5 @@ return [
                 'queues' => ['default'],
             ],
         ],
-
     ],
 ];
