ebeac080759a27b4a5f93c897df68f879ab63444
Volodymyr Morozov <v.morozov@promodo.com>
2025-08-15 14:04:13 +0300

    changed the default prefix from 'laravel' to 'app' to make all app metrics more universal

--- config/laravel-prometheus.php
+++ config/laravel-prometheus.php
@@ -2,7 +2,7 @@
 
 return [
     // namespace is used to prefix the prometheus metrics, can be any string value, but it is recommended to keep a default value
-    'namespace' => env('PROMETHEUS_NAMESPACE', 'laravel'),
+    'namespace' => env('PROMETHEUS_NAMESPACE', 'app'),
     // type of storage for keeping metrics data (known values: redis, in_memory, apcu). redis - works best now.
     'storage_type' => env('PROMETHEUS_STORAGE_TYPE', 'redis'),
     // redis connection for prometheus metrics (used only when storage_type == redis)
