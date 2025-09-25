251177aa94b686de398d5eb832146b33ab9adc77
Volodymyr Morozov <v.morozov@promodo.com>
2025-09-25 15:54:12 +0300

    fixed pushed jobs counter metric collector on laravel 10

--- src/Collectors/DefaultMetrics/QueuePushedJobsCounterMetricCollector.php
+++ src/Collectors/DefaultMetrics/QueuePushedJobsCounterMetricCollector.php
@@ -16,8 +16,21 @@ class QueuePushedJobsCounterMetricCollector
     {
         $this->metric->increment([
             'connection_name' => $event->connectionName,
-            'queue_name' => $event->queue,
+            'queue_name' => $this->resolveQueueName($event),
             'queue_job_name' => get_class($event->job),
         ]);
+    }
+
+    private function resolveQueueName(JobQueued $event): string
+    {
+        if (isset($event->queue)) {
+            return $event->queue;
+        }
+
+        if (method_exists($event->job, 'queue')) {
+            return $event->job->queue();
+        }
+
+        return $event->job->queue ?? config('queue.connections.redis.queue') ?? 'default';
     }
 }
